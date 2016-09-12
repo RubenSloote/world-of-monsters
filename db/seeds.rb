f98c37860fa07c8e1db8c8a88c6d2d09319e8d50
RubenSloote <rubensloote@gmail.com>
2016-09-12 10:11:44 +0200

    show monster

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Monster.create( { name:"Dracula", age: 357, image_url: "http://www.mydracula.com/wp-content/uploads/2015/09/dracula-untold.jpg" } )
+Monster.create( { name:"Gremlin", age: 56, image_url: "http://vignette4.wikia.nocookie.net/gremlins/images/4/40/Gremlins-560-dante.jpg/revision/latest?cb=20120218023934" } )
+Monster.create( { name:"Tinky Winky", age: 5 , image_url: "http://vignette3.wikia.nocookie.net/films-tv-shows-and-wildlife/images/c/c9/Tinky-Winky'sHeavyBag8.png/revision/latest?cb=20150414005952" } )
